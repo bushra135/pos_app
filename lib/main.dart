dee39c5eea7225663442a20b8e173b7938a6ab7c
bushra135 <202210140@stu.uob.edu.bh>
2026-04-09 17:02:57 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'POS App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('POS App'),
+        ),
+        body: const Center(
+          child: Text('Welcome POS App 🚀'),
+        ),
+      ),
+    );
+  }
+}
